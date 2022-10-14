dc291d23192a6a61913c085bc1aeebc850137c39
balogun Lukman <lukheebalo@gmail.com>
2022-10-14 02:28:58 +0100

    Feat: wiki search using click function

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 import json
+import click
 import wikipedia
 
 # prints when function loads
 print('Loading function')
 
-
-def lambda_handler(event, context):
+@click.command()
+@click.argument('entity')
+def main(entity):
     ''' Wikipedia page summarizer.
-        :param event: a request with a wikipedia "entity" that has page information
+        :param entity: a request with a wikipedia "entity" that has page information
         :return: a response that contains the first sentence of a wikipedia page,
             the response is JSON formatted.'''
-    
-    ## TO DO: Check that the request has some input body and save it
-    if 'body' in event:
-        event = json.loads(['body'])
 
-    #save the entity request value
-    entity = event['entity']
+    click.secho(f"Your Argument `{entity}`", bg="white", fg="black")
 
     # Set Status Code
     BAD_STATUS_CODE = 400
     GOOD_STATUS_CODE = 200 
+    COLOR_CODE = 'green'
 
     # Exception handling
     try:
@@ -28,23 +26,29 @@ def lambda_handler(event, context):
         statusCode = GOOD_STATUS_CODE
     except wikipedia.exceptions.PageError:
         res= f'\nThis word -{entity}- does not exist!\n'
+        COLOR_CODE = 'red'
         statusCode = BAD_STATUS_CODE
     except wikipedia.exceptions.DisambiguationError: 
         statusCode = BAD_STATUS_CODE
+        COLOR_CODE = 'red'
         res = f'\nThere are multiple references to this word {entity}!\n'
     except:
         statusCode = BAD_STATUS_CODE
+        COLOR_CODE = 'red'
         res = "\nSorry, Cannot Handle this request!\n"
 
     # print statements
-    print(f"context: {context}, event: {event}")
-    print(f"Response from wikipedia API: {res}")
+    click.secho("WIkI Response is", fg=COLOR_CODE)
+    click.secho(res, blink=True, bold=True)
     
     ## TO DO: Format the response as JSON and return the result
     response = {
         "statusCode": statusCode, 
-        "headers": { "Content-type": "application/json" },
+        # "headers": { "Content-type": "application/json" },
         "body": json.dumps({"message": res})
     }
     
     return response
+
+if __name__=="__main__":
+    main()
